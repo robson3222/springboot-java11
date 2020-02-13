18226382e0d4c5bc2950393eb16272699770d202
robson322 <robson3222@gmail.com>
2020-02-12 23:30:14 -0300

    Order, Instant, ISO 8601 parte2

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -11,24 +11,27 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 @Entity
 @Table(name = "td_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
-	 @Id
-	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant monent;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
-		
+
 	}
 
 	public Order(Long id, Instant monent, User client) {
@@ -87,5 +90,4 @@ public class Order implements Serializable {
 		return true;
 	}
 
-	
 }
